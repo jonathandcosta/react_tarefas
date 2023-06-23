80687a60e9f4b6d335e3efa684239e274967aa48
Jonathan Costa <jonathanduartecosta@gmail.com>
2023-06-22 22:19:39 -0300

    criando botão de navegação para nova tarefa/rota

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import EstiloGlobal, { Container } from './styles'
 
 import store from './store'
 import Home from './pages/Home'
+import Cadastro from './pages/Cadastro'
 
 const rotas = createBrowserRouter([
   {
@@ -12,7 +13,7 @@ const rotas = createBrowserRouter([
   },
   {
     path: '/novo',
-    element: <h1>nova tarefa</h1>
+    element: <Cadastro />
   }
 ])
 
